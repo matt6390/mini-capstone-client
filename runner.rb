237d329e08e8dad6daef2caf10fab3adc730c0a2
matt6390 <matthewstone639@gmail.com>
2018-01-16 21:24:47 -0600

    End of day backup

--- runner.rb
+++ runner.rb
@@ -1,4 +1,6 @@
 require 'unirest'
+require 'paint'
+
 puts
 puts "Press Enter to list all available products"
 puts "=" * 50
@@ -6,4 +8,7 @@ x = gets.chomp
 
 response = Unirest.get("http://localhost:3000/all_available_products")
 product_data = response.body
+
+puts Paint['These are my products', :red]
+
 puts JSON.pretty_generate(product_data)
