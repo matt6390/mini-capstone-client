a880fea4c04314272b81040f35f399df38adf34c
matt6390 <matthewstone639@gmail.com>
2018-01-18 20:29:58 -0600

    The client is now able to create a new entry in the datebase by entering in information in a form

--- runner.rb
+++ runner.rb
@@ -1,14 +1,47 @@
 require 'unirest'
-require 'paint'
 
-puts
-puts "Press Enter to list all available products"
-puts "=" * 50
-x = gets.chomp
+system "clear"
 
-response = Unirest.get("http://localhost:3000/all_available_products")
-product_data = response.body
+puts "Welcome to My Mini Capstone"
+puts "=" * 80
+puts "     Press [1] For all products"
+puts "     Press [2] To add a new product"
+puts "     Press [3] To find a specific product"
 
-puts Paint['These are my products', :red]
+input_option = gets.chomp
 
-puts JSON.pretty_generate(product_data)
+if input_option == "1"
+  response = Unirest.get("http://localhost:3000/products")
+  products = response.body
+  puts JSON.pretty_generate(products)
+elsif input_option == "2"
+  client_params = {}
+
+  print "Name: "
+  client_params[:name] = gets.chomp
+
+  print "Price: "
+  client_params[:price] = gets.chomp
+
+  print "Image URL: "
+  client_params[:image_url] = gets.chomp
+
+  print "Description: "
+  client_params[:description] = gets.chomp
+
+  response = Unirest.post(
+                        "http://localhost:3000/products",
+                        parameters: client_params
+                        )
+  product = response.body
+  puts JSON.pretty_generate(product)
+elsif input_option == "3"
+  print "Which product would you like to see? (Please enter a number)"
+  product_id = gets.chomp
+
+  response = Unirest.get("http://localhost:3000/products/#{product_id}")
+
+  product = response.body
+
+  puts JSON.pretty_generate(product)
+end
