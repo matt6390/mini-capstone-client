ea1e177d489771622adff347fdad3e11afc59734
matt6390 <matthewstone639@gmail.com>
2018-01-18 21:30:26 -0600

    The client can now work with the app to index, create, show, update, and destroy

--- runner.rb
+++ runner.rb
@@ -7,6 +7,8 @@ puts "=" * 80
 puts "     Press [1] For all products"
 puts "     Press [2] To add a new product"
 puts "     Press [3] To find a specific product"
+puts "     Press [4] to update a product"
+puts "     Press [5] to destroy a product entry"
 
 input_option = gets.chomp
 
@@ -44,4 +46,51 @@ elsif input_option == "3"
   product = response.body
 
   puts JSON.pretty_generate(product)
+
+elsif input_option == "4"
+  client_params = {}
+  print "Enter the ID of the product you'd like to update"
+  input_id = gets.chomp
+
+  print "Name: "
+  client_params[:name] = gets.chomp
+
+  print "Price: "
+  client_params[:price] = gets.chomp
+
+  print "Image URL: "
+  client_params[:image_url] = gets.chomp
+
+  print "Description: "
+  client_params[:description] = gets.chomp
+
+  response = Unirest.patch(
+                        "http://localhost:3000/products/#{input_id}",
+                        parameters: client_params
+                        )
+  product = response.body
+  puts JSON.pretty_generate(product)
+
+elsif input_option == "5"
+  print "Enter product id: "
+  input_id = gets.chomp
+
+  response = Unirest.delete("http://localhost:3000/products/#{input_id}")
+
+  data = response.body
+  puts JSON.pretty_generate(data)
+
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
